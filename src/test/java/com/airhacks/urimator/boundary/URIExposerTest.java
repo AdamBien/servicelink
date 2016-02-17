dff7b9de02d78d517a7cf00c27cfc7bf78bdfd11
Adam Bien <abien@adam-bien.com>
2016-02-17 15:31:39 +0100

    webtarget injection is working

--- src/test/java/com/airhacks/urimator/boundary/URIExposerTest.java
+++ src/test/java/com/airhacks/urimator/boundary/URIExposerTest.java
@@ -1,12 +1,14 @@
 package com.airhacks.urimator.boundary;
 
+import java.net.MalformedURLException;
+import javax.inject.Inject;
+import javax.ws.rs.client.WebTarget;
 import org.apache.deltaspike.testcontrol.api.junit.CdiTestRunner;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 
 /**
  *
@@ -15,12 +17,23 @@ import static org.junit.Assert.assertThat;
 @RunWith(CdiTestRunner.class)
 public class URIExposerTest {
 
+    @Inject
     @LinkedContainer(linkName = "ping", portNumber = 8080, resource = "/hugo")
     String fullyConfiguredHost;
 
+    @Inject
+    @LinkedContainer(linkName = "ping", portNumber = 8080, resource = "/hugo")
+    WebTarget fullyConfiguredWebTarget;
+
     @Test
-    public void linkAndPortAndResourceAreSet() {
+    public void fullyConfiguredURIInjection() {
         assertThat(fullyConfiguredHost, is("http://42.42.42.42:8080/hugo"));
+    }
+
+    @Test
+    public void fullyConfiguredWebTargetInjection() throws MalformedURLException {
+        assertNotNull(fullyConfiguredWebTarget);
+        assertThat(fullyConfiguredWebTarget.getUri().toString(), is("http://42.42.42.42:8080/hugo"));
     }
 
 }
